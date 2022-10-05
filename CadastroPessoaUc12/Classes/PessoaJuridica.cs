bc6dc92dd1f8a2c37d4c96fe808422a1898428a5
AFRANIO DIMAS DA SILVA <86030954+AfranioD@users.noreply.github.com>
2022-10-04 22:00:46 -0300

    Add files via upload
    Adicionado arquivos referentes as atividades do ER3-SA2 da Uc12

---
+++ CadastroPessoaUc12/Classes/PessoaJuridica.cs
@@ -0,0 +1,24 @@
+using CadastroPessoaUc12.Interfaces;
+
+namespace CadastroPessoaUc12.Classes
+{
+    public class PessoaJuridica : Pessoa, IPessoaJuridica
+    {
+
+        public string? Cnpj { get; set; }
+
+        public string? RazaoSocial { get; set; }
+
+
+
+        public override float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarCnpj(string cnpj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CadastroPessoaUc12/Program.cs
@@ -0,0 +1,27 @@
+﻿using CadastroPessoaUc12.Classes;
+
+PessoaFisica novaPf =new PessoaFisica();
+PessoaFisica metodosPf = new PessoaFisica();
+Endereco novoEndPf = new Endereco();
+
+novaPf.Nome = "Afranio Dimas";
+novaPf.dataNasc = new DateTime(2000, 01, 01);
+novaPf.Cpf = "1234567890";
+novaPf.Rendimento = 8500.5f;
+
+novoEndPf.Logradouro = "5th Avenue Park";
+novoEndPf.numero = 1485;
+novoEndPf.complemento = "5th on the Park";
+novoEndPf.endComercial = true;
+
+novaPf.Endereco = novoEndPf;
+
+Console.WriteLine(@$"
+Nome: {novaPf.Nome}
+Endereço: {novaPf.Endereco.Logradouro}, {novaPf.Endereco.numero}
+Maior de idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
+");
+
+// Console.WriteLine(novaPf.Nome);
+
+// Console.WriteLine($"Nome: {novaPf.Nome}");
